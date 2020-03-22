a2986bc16c4f877ded4db1498f2fe4f47402a18f
Mac Hale <machaledesign@gmail.com>
2020-03-22 14:53:31 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import AddBookmark from './AddBookmark/AddBookmark';
 import BookmarkList from './BookmarkList/BookmarkList';
 import Nav from './Nav/Nav';
 import config from './config';
 import './App.css';
+import Rating from './Rating/Rating'
 
 const bookmarks = [
   // {
@@ -31,20 +33,14 @@ const bookmarks = [
 
 class App extends Component {
   state = {
-    page: 'list',
     bookmarks,
     error: null,
   };
-
-  changePage = (page) => {
-    this.setState({ page })
-  }
 
   setBookmarks = bookmarks => {
     this.setState({
       bookmarks,
       error: null,
-      page: 'list',
     })
   }
 
@@ -73,23 +69,40 @@ class App extends Component {
   }
 
   render() {
-    const { page, bookmarks } = this.state
+    const { bookmarks } = this.state
     return (
       <main className='App'>
         <h1>Bookmarks!</h1>
-        <Nav clickPage={this.changePage} />
+        <Nav />
         <div className='content' aria-live='polite'>
-          {page === 'add' && (
+          {/* {page === 'add' && (
             <AddBookmark
               onAddBookmark={this.addBookmark}
               onClickCancel={() => this.changePage('list')}
             />
-          )}
-          {page === 'list' && (
+          )} */}
+          <Route 
+            path='/add-bookmark'
+            render={({ history }) => {
+              return <AddBookmark
+                onAddBookmark={this.addBookmark}
+                onClickCancel={() => history.push('/')}
+              />
+            }}  
+          />
+          {/* {page === 'list' && (
             <BookmarkList
               bookmarks={bookmarks}
             />
-          )}
+          )} */}
+          <Route
+            exact path='/'
+            render={() => 
+              <BookmarkList
+                bookmarks={bookmarks}
+              />}
+          />
+          <Rating value='hello'/>
         </div>
       </main>
     );
--- src/BookmarkItem/BookmarkItem.js
+++ src/BookmarkItem/BookmarkItem.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Rating from '../Rating/Rating';
 import './BookmarkItem.css';
+import BookmarkList from '../BookmarkList/BookmarkList';
+import PropTypes from 'prop-types'
 
 export default function BookmarkItem(props) {
   return (
@@ -31,6 +33,34 @@ export default function BookmarkItem(props) {
   )
 }
 
+BookmarkItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: (props, propName, componentName) => {
+    // get the value of the prop
+    const prop = props[propName];
+
+    // do the isRequired check
+    if(!prop) {
+      return new Error(`${propName} is required in ${componentName}. Validation Failed`);
+    }
+
+    // check the type
+    if (typeof prop != 'string') {
+      return new Error(`Invalid prop, ${propName} is expected to be a string in ${componentName}. ${typeof prop} found.`);
+    }
+
+    // do the custom check here
+    // using a simple regex
+    if (prop.length < 5 || !prop.match(new RegExp(/^https?:\/\//))) {
+      return new Error(`Invalid prop, ${propName} must be min length 5 and begin http(s)://. Validation Failed.`);
+    }
+  },
+  rating: PropTypes.number,
+  description: PropTypes.string
+}
+
 BookmarkItem.defaultProps = {
   onClickDelete: () => {},
+  rating: 1,
+  description: ""
 }
